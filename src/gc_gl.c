mardy/opengx

commit 586168c91f314a8bf01f3efaac5c45d80eb43d27

Refactor indexed drawing into a separate draw_elements_general()

src/gc_gl.c
@@ -2152,6 +2152,63 @@ void _ogx_apply_state()
     glparamstate.dirty.all = 0;
 }
 
+static void draw_elements_general(uint8_t gxmode, int count, GLenum type,
+                                  const GLvoid *indices,
+                                  int ne, int color_provide, int texen)
+{
+    // Not using indices
+    GX_ClearVtxDesc();
+    if (glparamstate.cs.vertex_enabled)
+        GX_SetVtxDesc(GX_VA_POS, GX_DIRECT);
+    if (ne)
+        GX_SetVtxDesc(GX_VA_NRM, GX_DIRECT);
+    if (color_provide)
+        GX_SetVtxDesc(GX_VA_CLR0, GX_DIRECT);
+    if (color_provide == 2)
+        GX_SetVtxDesc(GX_VA_CLR1, GX_DIRECT);
+    if (texen)
+        GX_SetVtxDesc(GX_VA_TEX0, GX_DIRECT);
+
+    // Using floats
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_F32, 0);
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_NRM, GX_NRM_XYZ, GX_F32, 0);
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR0, GX_CLR_RGBA, GX_RGBA8, 0);
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR1, GX_CLR_RGBA, GX_RGBA8, 0);
+
+    // Invalidate vertex data as may have been modified by the user
+    GX_InvVtxCache();
+
+    bool loop = (gxmode == GL_LINE_LOOP);
+    GX_Begin(gxmode, GX_VTXFMT0, count + loop);
+    for (int i = 0; i < count + loop; i++) {
+        int index = read_index(indices, type, i % count);
+        float value[4];
+        _ogx_array_reader_read_float(&glparamstate.vertex_array, index, value);
+
+        GX_Position3f32(value[0], value[1], value[2]);
+
+        if (ne) {
+            _ogx_array_reader_read_float(&glparamstate.normal_array, index, value);
+            GX_Normal3f32(value[0], value[1], value[2]);
+        }
+
+        if (color_provide) {
+            _ogx_array_reader_read_float(&glparamstate.color_array, index, value);
+            unsigned char arr[4] = { value[0] * 255.0f, value[1] * 255.0f, value[2] * 255.0f, value[3] * 255.0f };
+            GX_Color4u8(arr[0], arr[1], arr[2], arr[3]);
+            if (color_provide == 2)
+                GX_Color4u8(arr[0], arr[1], arr[2], arr[3]);
+        }
+
+        if (texen) {
+            _ogx_array_reader_read_float(&glparamstate.texcoord_array, index, value);
+            GX_TexCoord2f32(value[0], value[1]);
+        }
+    }
+    GX_End();
+}
+
 void glDrawArrays(GLenum mode, GLint first, GLsizei count)
 {
     unsigned char gxmode = draw_mode(mode);
@@ -2211,58 +2268,8 @@ void glDrawElements(GLenum mode, GLsizei count, GLenum type, const GLvoid *indic
             color_provide = 1;
     }
 
-    // Not using indices
-    GX_ClearVtxDesc();
-    if (glparamstate.cs.vertex_enabled)
-        GX_SetVtxDesc(GX_VA_POS, GX_DIRECT);
-    if (glparamstate.cs.normal_enabled)
-        GX_SetVtxDesc(GX_VA_NRM, GX_DIRECT);
-    if (color_provide)
-        GX_SetVtxDesc(GX_VA_CLR0, GX_DIRECT);
-    if (color_provide == 2)
-        GX_SetVtxDesc(GX_VA_CLR1, GX_DIRECT);
-    if (texen)
-        GX_SetVtxDesc(GX_VA_TEX0, GX_DIRECT);
-
-    // Using floats
-    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_F32, 0);
-    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_NRM, GX_NRM_XYZ, GX_F32, 0);
-    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
-    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR0, GX_CLR_RGBA, GX_RGBA8, 0);
-    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR1, GX_CLR_RGBA, GX_RGBA8, 0);
-
-    // Invalidate vertex data as may have been modified by the user
-    GX_InvVtxCache();
-
-    bool loop = (mode == GL_LINE_LOOP);
-    GX_Begin(gxmode, GX_VTXFMT0, count + loop);
-    int i;
-    for (i = 0; i < count + loop; i++) {
-        int index = read_index(indices, type, i % count);
-        float value[4];
-        _ogx_array_reader_read_float(&glparamstate.vertex_array, index, value);
-
-        GX_Position3f32(value[0], value[1], value[2]);
-
-        if (glparamstate.cs.normal_enabled) {
-            _ogx_array_reader_read_float(&glparamstate.normal_array, index, value);
-            GX_Normal3f32(value[0], value[1], value[2]);
-        }
-
-        if (color_provide) {
-            _ogx_array_reader_read_float(&glparamstate.color_array, index, value);
-            unsigned char arr[4] = { value[0] * 255.0f, value[1] * 255.0f, value[2] * 255.0f, value[3] * 255.0f };
-            GX_Color4u8(arr[0], arr[1], arr[2], arr[3]);
-            if (color_provide == 2)
-                GX_Color4u8(arr[0], arr[1], arr[2], arr[3]);
-        }
-
-        if (texen) {
-            _ogx_array_reader_read_float(&glparamstate.texcoord_array, index, value);
-            GX_TexCoord2f32(value[0], value[1]);
-        }
-    }
-    GX_End();
+    draw_elements_general(gxmode, count, type, indices,
+                          glparamstate.cs.normal_enabled, color_provide, texen);
 }
 
 static void draw_arrays_general(uint8_t gxmode, int first, int count, int ne,

commit 45c3cbbc747a7511700e60d8dceeb13ac534e4ab

draw: restore drawing of loop lines
This got broken in 586168c91f314a8bf01f3efaac5c45d80eb43d27, which
changed the code to compare a GX mode (an 8-bit integer) with
GL_LINE_LOOP (which is a GLenum, 16-bit wide).

We fix this by refactoring the draw_mode() function to return a small
struct containing both the GX mode and a flag to indicate if looping is
needed.

src/gc_gl.c
@@ -69,11 +69,17 @@ typedef struct
     uint8_t specular_mask;
 } LightMasks;
 
+typedef struct
+{
+    uint8_t mode;
+    bool loop;
+} DrawMode;
+
 char _ogx_log_level = 0;
 static GXTexObj s_zbuffer_texture;
 static uint8_t s_zbuffer_texels[2 * 32] ATTRIBUTE_ALIGN(32);
 
-static void draw_arrays_general(uint8_t gxmode, int first, int count, int ne,
+static void draw_arrays_general(DrawMode gxmode, int first, int count, int ne,
                                 int color_provide, int texen);
 
 static inline void update_modelview_matrix()
@@ -1714,38 +1720,38 @@ static LightMasks prepare_lighting()
     return masks;
 }
 
-static unsigned char draw_mode(GLenum mode)
+static DrawMode draw_mode(GLenum mode)
 {
-    unsigned char gxmode;
+    DrawMode dm = { 0xff, false };
     switch (mode) {
     case GL_POINTS:
-        gxmode = GX_POINTS;
+        dm.mode = GX_POINTS;
         break;
     case GL_LINE_LOOP:
+        dm.loop = true;
+        // fall through
     case GL_LINE_STRIP:
-        gxmode = GX_LINESTRIP;
+        dm.mode = GX_LINESTRIP;
         break;
     case GL_LINES:
-        gxmode = GX_LINES;
+        dm.mode = GX_LINES;
         break;
     case GL_TRIANGLE_STRIP:
     case GL_QUAD_STRIP:
-        gxmode = GX_TRIANGLESTRIP;
+        dm.mode = GX_TRIANGLESTRIP;
         break;
     case GL_TRIANGLE_FAN:
     case GL_POLYGON:
-        gxmode = GX_TRIANGLEFAN;
+        dm.mode = GX_TRIANGLEFAN;
         break;
     case GL_TRIANGLES:
-        gxmode = GX_TRIANGLES;
+        dm.mode = GX_TRIANGLES;
         break;
     case GL_QUADS:
-        gxmode = GX_QUADS;
+        dm.mode = GX_QUADS;
         break;
-    default:
-        return 0xff; // FIXME: Emulate these modes
     }
-    return gxmode;
+    return dm;
 }
 
 static void setup_fog()
@@ -2112,7 +2118,7 @@ void _ogx_apply_state()
 }
 
 typedef struct {
-    u8 gxmode;
+    DrawMode gxmode;
     GLint first;
     GLsizei count;
 } OgxDrawData;
@@ -2130,7 +2136,7 @@ static void flat_draw_geometry(void *cb_data)
     GX_End();
 }
 
-static void draw_elements_general(uint8_t gxmode, int count, GLenum type,
+static void draw_elements_general(DrawMode gxmode, int count, GLenum type,
                                   const GLvoid *indices,
                                   int ne, int color_provide, int texen)
 {
@@ -2157,8 +2163,8 @@ static void draw_elements_general(uint8_t gxmode, int count, GLenum type,
     // Invalidate vertex data as may have been modified by the user
     GX_InvVtxCache();
 
-    bool loop = (gxmode == GL_LINE_LOOP);
-    GX_Begin(gxmode, GX_VTXFMT0, count + loop);
+    bool loop = gxmode.loop;
+    GX_Begin(gxmode.mode, GX_VTXFMT0, count + loop);
     for (int i = 0; i < count + loop; i++) {
         int index = read_index(indices, type, i % count);
         float value[4];
@@ -2188,7 +2194,7 @@ static void draw_elements_general(uint8_t gxmode, int count, GLenum type,
 }
 
 typedef struct {
-    u8 gxmode;
+    DrawMode gxmode;
     GLsizei count;
     GLenum type;
     const GLvoid *indices;
@@ -2234,8 +2240,8 @@ void glArrayElement(GLint i)
 
 void glDrawArrays(GLenum mode, GLint first, GLsizei count)
 {
-    unsigned char gxmode = draw_mode(mode);
-    if (gxmode == 0xff)
+    DrawMode gxmode = draw_mode(mode);
+    if (gxmode.mode == 0xff)
         return;
 
     bool should_draw = true;
@@ -2276,9 +2282,8 @@ void glDrawArrays(GLenum mode, GLint first, GLsizei count)
 
 void glDrawElements(GLenum mode, GLsizei count, GLenum type, const GLvoid *indices)
 {
-
-    unsigned char gxmode = draw_mode(mode);
-    if (gxmode == 0xff)
+    DrawMode gxmode = draw_mode(mode);
+    if (gxmode.mode == 0xff)
         return;
 
     bool should_draw = true;
@@ -2316,7 +2321,7 @@ void glDrawElements(GLenum mode, GLsizei count, GLenum type, const GLvoid *indic
     }
 }
 
-static void draw_arrays_general(uint8_t gxmode, int first, int count, int ne,
+static void draw_arrays_general(DrawMode gxmode, int first, int count, int ne,
                                 int color_provide, int texen)
 {
     // Not using indices
@@ -2342,8 +2347,8 @@ static void draw_arrays_general(uint8_t gxmode, int first, int count, int ne,
     // Invalidate vertex data as may have been modified by the user
     GX_InvVtxCache();
 
-    bool loop = (gxmode == GL_LINE_LOOP);
-    GX_Begin(gxmode, GX_VTXFMT0, count + loop);
+    bool loop = gxmode.loop;
+    GX_Begin(gxmode.mode, GX_VTXFMT0, count + loop);
     int i;
     for (i = 0; i < count + loop; i++) {
         int j = i % count + first;