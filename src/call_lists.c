a4afaecbfa7941ed1d46ebaab8edfe6a808da0cf
Alberto Mardegan <mardy@users.sourceforge.net>
2024-10-10 17:57:59 +0300

    WIP lists

--- src/call_lists.c
+++ src/call_lists.c
@@ -105,6 +105,14 @@ typedef struct
             GLfloat z;
         } rotate;
 
+        struct DrawGeometry {
+            GLenum mode;
+            uint16_t count;
+            struct client_state cs;
+            u32 list_size;
+            void *gxlist;
+        } draw_geometry;
+
         float color[4];
 
         float normal[3];
@@ -125,6 +133,9 @@ typedef struct
 } CallList;
 
 static CallList call_lists[MAX_CALL_LISTS];
+static GXColor s_current_color ATTRIBUTE_ALIGN(32);
+typedef float NormalType[3];
+static NormalType s_current_normals[2] ATTRIBUTE_ALIGN(32);
 
 #define BUFFER_IS_VALID(buffer) (((uint32_t)buffer) > 1)
 #define LIST_IS_USED(index) BUFFER_IS_VALID(call_lists[index].head)
@@ -213,11 +224,105 @@ static void run_gx_list(struct GXDisplayList *gxlist)
     }
 }
 
+static void execute_draw_geometry_list(struct DrawGeometry *dg)
+{
+    static int counter = 0;
+    float *normals_array;
+    u8 vtxindex = GX_VTXFMT0;
+    if (counter++ % 2) {
+        normals_array = s_current_normals[1];
+    } else {
+        normals_array = s_current_normals[0];
+    }
+    GX_ClearVtxDesc();
+    GX_SetVtxDesc(GX_VA_POS, GX_DIRECT);
+    if (dg->cs.normal_enabled) {
+        GX_SetVtxDesc(GX_VA_NRM, GX_DIRECT);
+    } else {
+        GX_SetVtxDesc(GX_VA_NRM, GX_INDEX8);
+        fprintf(stderr, "normals: %p\n", normals_array);
+        GX_SetArray(GX_VA_NRM, normals_array, 12);
+        floatcpy(normals_array, glparamstate.imm_mode.current_normal, 3);
+        /* Not needed on Dolphin, but it is on a Wii */
+        DCFlushRange(normals_array, 12);
+    }
+    if (dg->cs.color_enabled) {
+        GX_SetVtxDesc(GX_VA_CLR0, GX_DIRECT);
+        GX_SetVtxDesc(GX_VA_CLR1, GX_DIRECT);
+    } else {
+        GX_SetVtxDesc(GX_VA_CLR0, GX_INDEX8);
+        GX_SetVtxDesc(GX_VA_CLR1, GX_INDEX8);
+        s_current_color = gxcol_new_fv(glparamstate.imm_mode.current_color);
+        GX_SetArray(GX_VA_CLR0, &s_current_color, 4);
+        GX_SetArray(GX_VA_CLR1, &s_current_color, 4);
+        DCFlushRange(&s_current_color, 4);
+    }
+    /* It makes no sense to use a fixed texture coordinates for all vertices,
+     * so we won't add them unless they are enabled. */
+    if (dg->cs.texcoord_enabled) {
+        GX_SetVtxDesc(GX_VA_TEX0, GX_DIRECT);
+        GX_SetVtxAttrFmt(vtxindex, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
+    }
+
+    GX_SetVtxAttrFmt(vtxindex, GX_VA_POS, GX_POS_XYZ, GX_F32, 0);
+    GX_SetVtxAttrFmt(vtxindex, GX_VA_NRM, GX_NRM_XYZ, GX_F32, 0);
+    GX_SetVtxAttrFmt(vtxindex, GX_VA_CLR0, GX_CLR_RGBA, GX_RGBA8, 0);
+    GX_SetVtxAttrFmt(vtxindex, GX_VA_CLR1, GX_CLR_RGBA, GX_RGBA8, 0);
+    GX_InvVtxCache();
+
+    GX_CallDispList(dg->gxlist, dg->list_size);
+    /* Without this, the last few triangles risk not getting drawn (both on
+     * Dolphin and on the HW) */
+    //GX_DrawDone();
+}
+
+static void flat_draw_geometry(void *cb_data)
+{
+    struct DrawGeometry *dg = cb_data;
+    execute_draw_geometry_list(dg);
+}
+
+static void run_draw_geometry(struct DrawGeometry *dg)
+{
+    struct client_state cs;
+
+    /* Update the drawing mode on the list. This required peeping into
+     * GX_Begin() code. */
+    DrawMode gxmode = _ogx_draw_mode(dg->mode);
+    u8 *fifo_ptr = dg->gxlist;
+    *fifo_ptr = gxmode.mode | (GX_VTXFMT1 & 0x7);
+    // TODO: check if DCStoreRange() is needed
+
+    _ogx_efb_set_content_type(OGX_EFB_SCENE);
+
+    cs = glparamstate.cs;
+    glparamstate.cs = dg->cs;
+    _ogx_apply_state();
+    _ogx_setup_render_stages();
+    glparamstate.cs = cs;
+
+    execute_draw_geometry_list(dg);
+
+    glparamstate.draw_count++;
+
+    /*
+    if (glparamstate.stencil.enabled) {
+        _ogx_stencil_draw(flat_draw_geometry, dg);
+    }
+    */
+}
+
 static void run_command(Command *cmd)
 {
     switch (cmd->type) {
     case COMMAND_GXLIST:
         run_gx_list(&cmd->c.gxlist);
+        break;
+    case COMMAND_DRAW_ARRAYS:
+        run_draw_geometry(&cmd->c.draw_geometry);
+        break;
+    case COMMAND_DRAW_ELEMENTS:
+        run_draw_geometry(&cmd->c.draw_geometry);
         break;
     case COMMAND_CALL_LIST:
         glCallList(cmd->c.gllist);
@@ -324,6 +429,104 @@ static void close_list(int index)
 
 }
 
+typedef int (*IndexCallback)(int i, void *index_data);
+
+static int draw_array_index_cb(int i, void *index_data)
+{
+    int first = *(int*)index_data;
+    return i + first;
+}
+
+typedef struct {
+    GLenum type;
+    const GLvoid *indices;
+} DrawElementsIndexData;
+
+static int draw_elements_index_cb(int i, void *index_data)
+{
+    const DrawElementsIndexData *id = (DrawElementsIndexData*)index_data;
+    return read_index(id->indices, id->type, i);
+}
+
+static void queue_draw_geometry(struct DrawGeometry *dg,
+                                GLenum mode, GLsizei count,
+                                IndexCallback index_cb,
+                                void *index_data)
+{
+    /* When executing a display list containing glDrawElements() or
+     * glDrawArrays() all the attributes that were not enabled at the time of
+     * the list creation should be taken from the current active attribute
+     * (color, normals and texture coordinates). Since we are not be able to
+     * modify a GX list to add more attributes, we'll add them now as indexed
+     * attributes: this will allow us to set the value of the indexed attribute
+     * at the time when the list is executed. */
+    dg->mode = mode;
+    dg->gxlist = memalign(32, MAX_GXLIST_SIZE);
+    DCInvalidateRange(dg->gxlist, MAX_GXLIST_SIZE);
+    dg->cs = glparamstate.cs;
+    DrawMode gxmode = _ogx_draw_mode(mode);
+    dg->count = count + gxmode.loop;
+
+    GX_BeginDispList(dg->gxlist, MAX_GXLIST_SIZE);
+
+    /* Note that the drawing mode set here will be overwritten when executing the list */
+
+    GX_Begin(gxmode.mode, GX_VTXFMT1, dg->count);
+    for (int i = 0; i < dg->count; i++) {
+        int index = index_cb(i % count, index_data);
+        float value[4];
+        _ogx_array_reader_read_pos3f(&glparamstate.vertex_array, index, value);
+
+        GX_Position3f32(value[0], value[1], value[2]);
+
+        if (dg->cs.normal_enabled) {
+            _ogx_array_reader_read_norm3f(&glparamstate.normal_array, index, value);
+            GX_Normal3f32(value[0], value[1], value[2]);
+        } else {
+            GX_Normal1x8(0);
+        }
+
+        if (dg->cs.color_enabled) {
+            GXColor color;
+            _ogx_array_reader_read_color(&glparamstate.color_array, index, &color);
+            GX_Color4u8(color.r, color.g, color.b, color.a); // CLR0
+            GX_Color4u8(color.r, color.g, color.b, color.a); // CLR1
+        } else {
+            GX_Color1x8(0); // CLR0
+            GX_Color1x8(0); // CLR1
+        }
+
+        if (dg->cs.texcoord_enabled) {
+            _ogx_array_reader_read_tex2f(&glparamstate.texcoord_array, index, value);
+            GX_TexCoord2f32(value[0], value[1]);
+        }
+    }
+    GX_End();
+
+    u32 size = GX_EndDispList();
+    fprintf(stderr, "Created draw list %u\n", size);
+    /* Free any excess memory */
+    void *new_ptr = realloc(dg->gxlist, size);
+    assert(new_ptr == dg->gxlist);
+    dg->list_size = size;
+}
+
+static void queue_draw_arrays(struct DrawGeometry *dg,
+                              GLenum mode, GLint first, GLsizei count)
+{
+    queue_draw_geometry(dg, mode, count,
+                        draw_array_index_cb, &first);
+}
+
+static void queue_draw_elements(struct DrawGeometry *dg,
+                                GLenum mode, GLsizei count, GLenum type,
+                                const GLvoid *indices)
+{
+    DrawElementsIndexData id = { type, indices };
+    queue_draw_geometry(dg, mode, count,
+                        draw_elements_index_cb, &id);
+}
+
 static void destroy_buffer(CommandBuffer *buffer)
 {
     if (buffer->next) {
@@ -335,10 +538,14 @@ static void destroy_buffer(CommandBuffer *buffer)
         if (command->type == COMMAND_NONE) break;
 
         /* Free the memory for those commands who allocated it */
+        void *ptr = NULL;
         if (command->type == COMMAND_GXLIST) {
-            void *ptr = command->c.gxlist.list;
-            if (ptr) free(ptr);
+            ptr = command->c.gxlist.list;
+        } else if (command->type == COMMAND_DRAW_ELEMENTS ||
+                   command->type == COMMAND_DRAW_ARRAYS) {
+            ptr = command->c.draw_geometry.gxlist;
         }
+        if (ptr) free(ptr);
     }
     free(buffer);
 }
@@ -398,6 +605,25 @@ bool _ogx_call_list_append(CommandType op, ...)
     switch (op) {
     case COMMAND_CALL_LIST:
         command->c.gllist = va_arg(ap, GLuint);
+        break;
+    case COMMAND_DRAW_ARRAYS:
+        {
+            GLenum mode = va_arg(ap, GLenum);
+            GLint first = va_arg(ap, GLint);
+            GLsizei count = va_arg(ap, GLsizei);
+            queue_draw_arrays(&command->c.draw_geometry,
+                              mode, first, count);
+        }
+        break;
+    case COMMAND_DRAW_ELEMENTS:
+        {
+            GLenum mode = va_arg(ap, GLenum);
+            GLsizei count = va_arg(ap, GLsizei);
+            GLenum type = va_arg(ap, GLenum);
+            const GLvoid *indices = va_arg(ap, GLvoid *);
+            queue_draw_elements(&command->c.draw_geometry,
+                                mode, count, type, indices);
+        }
         break;
     case COMMAND_ENABLE:
     case COMMAND_DISABLE:
--- src/gc_gl.c
+++ src/gc_gl.c
@@ -72,12 +72,6 @@ typedef struct
     uint8_t specular_mask;
 } LightMasks;
 
-typedef struct
-{
-    uint8_t mode;
-    bool loop;
-} DrawMode;
-
 char _ogx_log_level = 0;
 static OgxEfbBuffer *s_efb_scene_buffer = NULL;
 static GXTexObj s_zbuffer_texture;
@@ -1867,7 +1861,7 @@ static LightMasks prepare_lighting()
     return masks;
 }
 
-static DrawMode draw_mode(GLenum mode)
+DrawMode _ogx_draw_mode(GLenum mode)
 {
     DrawMode dm = { 0xff, false };
 
@@ -2408,30 +2402,27 @@ void glArrayElement(GLint i)
 
 void glDrawArrays(GLenum mode, GLint first, GLsizei count)
 {
-    DrawMode gxmode = draw_mode(mode);
+    DrawMode gxmode = _ogx_draw_mode(mode);
     if (gxmode.mode == 0xff)
         return;
 
+    HANDLE_CALL_LIST(DRAW_ARRAYS, mode, first, count);
+
     bool should_draw = true;
     int texen = glparamstate.cs.texcoord_enabled;
-    if (glparamstate.current_call_list.index >= 0 &&
-        glparamstate.current_call_list.execution_depth == 0) {
-        _ogx_call_list_append(COMMAND_GXLIST);
-    } else {
-        if (glparamstate.stencil.enabled) {
-            OgxDrawData draw_data = { gxmode, first, count };
-            _ogx_stencil_draw(flat_draw_geometry, &draw_data);
-        }
-
-        _ogx_efb_set_content_type(OGX_EFB_SCENE);
-        should_draw = _ogx_setup_render_stages();
-        _ogx_apply_state();
-
-        /* When not building a display list, we can optimize the drawing by
-         * avoiding passing texture coordinates if texturing is not enabled.
-         */
-        texen = texen && glparamstate.texture_enabled;
+    if (glparamstate.stencil.enabled) {
+        OgxDrawData draw_data = { gxmode, first, count };
+        _ogx_stencil_draw(flat_draw_geometry, &draw_data);
     }
+
+    _ogx_efb_set_content_type(OGX_EFB_SCENE);
+    should_draw = _ogx_setup_render_stages();
+    _ogx_apply_state();
+
+    /* When not building a display list, we can optimize the drawing by
+     * avoiding passing texture coordinates if texturing is not enabled.
+     */
+    texen = texen && glparamstate.texture_enabled;
 
     if (should_draw) {
         int color_provide = 0;
@@ -2451,29 +2442,26 @@ void glDrawArrays(GLenum mode, GLint first, GLsizei count)
 
 void glDrawElements(GLenum mode, GLsizei count, GLenum type, const GLvoid *indices)
 {
-    DrawMode gxmode = draw_mode(mode);
+    DrawMode gxmode = _ogx_draw_mode(mode);
     if (gxmode.mode == 0xff)
         return;
 
+    HANDLE_CALL_LIST(DRAW_ELEMENTS, mode, count, type, indices);
+
     bool should_draw = true;
     int texen = glparamstate.cs.texcoord_enabled;
-    if (glparamstate.current_call_list.index >= 0 &&
-        glparamstate.current_call_list.execution_depth == 0) {
-        _ogx_call_list_append(COMMAND_GXLIST);
-    } else {
-        if (glparamstate.stencil.enabled) {
-            OgxDrawElementsData draw_data = { gxmode, count, type, indices };
-            _ogx_stencil_draw(flat_draw_elements, &draw_data);
-        }
-
-        _ogx_efb_set_content_type(OGX_EFB_SCENE);
-        should_draw = _ogx_setup_render_stages();
-        _ogx_apply_state();
-        /* When not building a display list, we can optimize the drawing by
-         * avoiding passing texture coordinates if texturing is not enabled.
-         */
-        texen = texen && glparamstate.texture_enabled;
+    if (glparamstate.stencil.enabled) {
+        OgxDrawElementsData draw_data = { gxmode, count, type, indices };
+        _ogx_stencil_draw(flat_draw_elements, &draw_data);
     }
+
+    _ogx_efb_set_content_type(OGX_EFB_SCENE);
+    should_draw = _ogx_setup_render_stages();
+    _ogx_apply_state();
+    /* When not building a display list, we can optimize the drawing by
+     * avoiding passing texture coordinates if texturing is not enabled.
+     */
+    texen = texen && glparamstate.texture_enabled;
 
     if (should_draw) {
         int color_provide = 0;
